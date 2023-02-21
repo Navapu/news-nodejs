18de138f458472b49d9e52522d2d45be1384e505
Navapu <drojannp2@gmail.com>
2023-02-21 17:41:29 +0100

    APP finalazada

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { engine } = require('express-handlebars')
 
 const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-// const newsRouter = require('./routes/news');
+const usersRouter = require('./routes/users');
+const newsRouter = require('./routes/news');
 
 const app = express();
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', engine({
-  defaultLayout: 'main',
-  layoutsDir:  path.join(app.get('views'), 'layouts'),
-  partialsDir:  path.join(app.get('views'), 'partials'),
-  extname: '.hbs',
-  helpers: require('./lib/handlebars')
-}))
+app.set('view engine', 'ejs');
 
-app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,21 +21,24 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/news', newsRouter);
+app.use('/users', usersRouter);
+app.use('/news', newsRouter);
 
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
- app.use(function(err, req, res, next) {
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   res.status(err.status || 500);
-   res.render('error');
- });
-app.listen(3000)
-console.log("Escuchando en el puerto 3000")
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 module.exports = app;
